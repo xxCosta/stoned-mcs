f44e00aee458344e8ed1d2b9e98c5be8ad5388e8
root <root@DESKTOP-QNL5HH1.localdomain>
2022-06-12 19:26:49 -0300

    moved to wretch for frontend http calls

--- frontend-install/src/App.js
+++ frontend-install/src/App.js
@@ -3,6 +3,12 @@ import './home.scss';
 function App() {
   return (
     <div className="App">
+      <div className="container">
+        <div className='fakeBlock'>HI from block</div>
+        <div className='fakeBlock'>HI from block</div>
+        <div className='fakeBlock'>HI from block</div>
+      </div>
+     <p>hello</p> 
     </div>
   );
 }
